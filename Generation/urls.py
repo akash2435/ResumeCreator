5f4550b9cc486ec69aa381da53a0dab58f73ec52
akash2435 <akash.19is008@cambridge.edu.in>
2022-10-03 12:07:21 +0530

    commit

--- Generation/urls.py
+++ Generation/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from Generation import views
 urlpatterns = [
-    path('home',views.home,name = 'home'),
-    path('create',views.create,name = 'create'),
-    path('steptwo',views.steptwo, name='steptwo')
+    path('',views.home,name = 'home'),
+    path('testing',views.testing, name='testing')
 ]
--- Generation/views.py
+++ Generation/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from django.template.loader import get_template
+
+from xhtml2pdf import pisa
 # Create your views here.
 
 def home(request):
@@ -10,3 +15,24 @@ def create(request):
 
 def steptwo(request):
     return render(request, 'create_next2.html')
+
+def testing(request):
+    return render(request, 'testing.html') 
+    template_path = 'testing.html'
+
+    response = HttpResponse(content_type='application/pdf')
+    context = {'testing' : "testing"}
+
+    response['Content-Disposition'] = 'filename="testing.pdf"'
+
+    template = get_template(template_path)
+
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
